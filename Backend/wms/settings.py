28906ee6ac2c6acfc365c1177bf60154675a3544
Nitin Daswani <nitindaswani771@gmail.com>
2025-12-10 10:04:19 +0530

    Add specific Netlify domain to CSRF origins

--- Backend/wms/settings.py
+++ Backend/wms/settings.py
@@ -163,6 +163,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5050',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://*.netlify.app'
+    'https://*.netlify.app',
+    'https://workshop-management.netlify.app'
 ]
 
